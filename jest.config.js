433db6c4cf07b3b5d82a1f240471b5e376aa9253
Alexander B <bas2784@gmail.com>
2023-06-01 15:10:28 +0300

    jest ch

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-export const moduleNameMapper = {
-  "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/empty-module.js",
+// eslint-disable-next-line no-undef
+module.exports = {
+  // ...другие настройки Jest...
+  moduleNameMapper: {
+    "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/empty-module.js",
+  },
+
+  testEnvironment: "jsdom",
 };
-export const testEnvironment = "jsdom";
